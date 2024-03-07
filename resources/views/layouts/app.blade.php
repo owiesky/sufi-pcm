ec59e0727c1fe5d7d399489659d8a0654864c78c
owiesky <owie.aryaguna@gmail.com>
2024-03-07 21:14:34 +0700

    new app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="valentine">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -16,24 +14,55 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen">
-        <livewire:layout.navigation />
+<body class="min-h-screen font-sans antialiased">
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header>
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
+    {{-- The navbar with `sticky` and `full-width` --}}
+    <x-mary-nav sticky full-width>
 
-        <!-- Page Content -->
-        <main>
+        <x-slot:brand>
+            {{-- Drawer toggle for "main-drawer" --}}
+            <label for="main-drawer" class="lg:hidden mr-3">
+                <x-mary-icon name="o-bars-3" class="cursor-pointer" />
+            </label>
+
+            {{-- Brand --}}
+            <div>App</div>
+        </x-slot:brand>
+
+        {{-- Right side actions --}}
+        <x-slot:actions>
+            <x-mary-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" />
+            <x-mary-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" />
+        </x-slot:actions>
+    </x-mary-nav>
+
+    {{-- The main content with `full-width` --}}
+    <x-mary-main with-nav full-width>
+
+        {{-- This is a sidebar that works also as a drawer on small screens --}}
+        {{-- Notice the `main-drawer` reference here --}}
+        <x-slot:sidebar drawer="main-drawer" class="bg-slate-200">
+
+            {{-- Activates the menu item when a route matches the `link` property --}}
+            <x-mary-menu activate-by-route>
+                <x-mary-menu-item title="Home" icon="o-home" link="###" />
+                <x-mary-menu-item title="Messages" icon="o-envelope" link="###" />
+            </x-mary-menu>
+        </x-slot:sidebar>
+
+        {{-- The `$slot` goes here --}}
+        <x-slot:content>
             {{ $slot }}
-        </main>
-    </div>
+        </x-slot:content>
+
+        {{-- Footer area --}}
+        <x-slot:footer>
+            <hr />
+            <div class="p-6">
+                Footer
+            </div>
+        </x-slot:footer>
+    </x-mary-main>
 </body>
 
 </html>
